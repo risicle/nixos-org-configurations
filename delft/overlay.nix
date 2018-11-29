369493ccc11a8390cd355bb97b4369b94e06fa3a
graham christensen <graham@grahamc.com>
2018-11-29 01:52:49 +0100

    Add an 'upgrade' helper which helps enforce upgrades are upgrades

--- delft/overlay.nix
+++ delft/overlay.nix
@@ -1,6 +1,19 @@
 self: super:
-{
-  nixUnstable = super.nixUnstable.overrideAttrs (oldatrs: {
+let
+  upgrade = package: overrides:
+    let
+      upgraded = package.overrideAttrs overrides;
+      upgradedVersion = (builtins.parseDrvName upgraded.name).version;
+      originalVersion =(builtins.parseDrvName package.name).version;
+
+      isDowngrade = (builtins.compareVersions upgradedVersion originalVersion) == -1;
+
+      warn = builtins.trace
+        "Warning: ${package.name} downgraded by overlay with ${upgraded.name}.";
+      pass = x: x;
+    in (if isDowngrade then warn else pass) upgraded;
+in {
+  nixUnstable = upgrade super.nixUnstable (oldAttrs: {
     name = "nix-2.2pre6526_9f99d624";
     src = self.fetchFromGitHub {
       owner = "NixOS";
